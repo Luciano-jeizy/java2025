184ab5cf63db45868767aeac959bdc7ae13eb962
jeizy-luciano <164494003+Luciano-jeizy@users.noreply.github.com>
2025-09-23 15:02:28 +0100

    Add files via upload

---
+++ Atividade_3/src/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+    public static void main (String[]args){
+
+        int preco = 9;
+
+        if(preco < 10){
+            System.out.println("O livro é barato.");
+        }
+         else{
+            System.out.println("O livro é caro.");
+
+        }
+    }
+}
+
+
