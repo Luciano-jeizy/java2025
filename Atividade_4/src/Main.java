c998a56170bbd549e9e5d5af59de4d1dd00c55d3
jeizy-luciano <164494003+Luciano-jeizy@users.noreply.github.com>
2025-09-25 15:34:12 +0100

    Add files via upload

---
+++ Atividade_4/src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        char vogal = 'B';
+
+
+        switch (vogal) {
+            case 'A':
+                System.out.println("É uma vogal.");
+                break;
+            case 'E':
+                System.out.println("É uma vogal.");
+                break;
+
+            case 'I':
+                System.out.println("É uma vogal.");
+                break;
+
+            case 'O':
+                System.out.println("É uma vogal.");
+                break;
+            case 'U':
+                System.out.println("É uma vogal.");
+                break;
+
+            default:
+                System.out.println("É uma consoante.");
+                break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
