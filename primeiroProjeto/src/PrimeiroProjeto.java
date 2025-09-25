4c31a05a71b38c0f9aab9b90426c2a21a15df9fe
jeizy-luciano <164494003+Luciano-jeizy@users.noreply.github.com>
2025-09-25 15:36:01 +0100

    Add files via upload

---
+++ primeiroProjeto/src/PrimeiroProjeto.java
@@ -0,0 +1,17 @@
+public class PrimeiroProjeto {
+    public static void main (String [] args) {
+       String nome = "Diego";
+       String morada = "Rua de Matadouro";
+       int idade = 20;
+       char estadocivil = 'C';
+       double media_das_notas= 18+17;
+
+       System.out.println("Nome:"+ nome);
+        System.out.println("Morada:"+ morada);
+        System.out.println("Idade:"+ idade);
+        System.out.println("Estadocivil:"+ estadocivil);
+        System.out.println("Media_das_notas:"+ media_das_notas);
+
+        System.out.println("Nome:"+ nome + ", Morada:"+ morada+ ", Idade:"+ idade + ", Estadocivil:"+ estadocivil + ", Media_das_notas:"+ media_das_notas);
+    }
+}
