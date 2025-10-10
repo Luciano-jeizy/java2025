ea1303215d762224f288b74041c68f8b03d94e2c
jeizy-luciano <164494003+Luciano-jeizy@users.noreply.github.com>
2025-10-10 09:22:16 +0100

    Add files via upload

---
+++ atividade_13/src/Atividade_13.java
@@ -0,0 +1,12 @@
+public class Atividade_13 {
+    public static void main (String [] args){
+
+        int grau = 90;
+
+        if(grau >= 100) {
+            System.out.println("A água está a ferver a temperatura de 100ºc!");
+        } else {
+            System.out.println("A água não está a ferver!");
+        }
+    }
+}
